78eaadf2c2a22264f70e675d1fcf5ab77e176c94
tsui <tsui.uestc@gmail.com>
2013-01-06 10:07:55 +0800

    relevance analysis

---
+++ relevance/crawl.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# 
+# filename: crawl.py
+# author: tsui(tsui.uestc@gmail.com)
+# date: 2013-01-05
+#
+import string
+import urllib2
+from sgmllib import SGMLParser
+
+#Open files
+file_tic = open('ticker.txt')
+file_data = open('data.txt')
+
+#temp vars
+line_tic = []
+line_data = []
+tic_tmp = []
+
+#read ticker
+try:
+  while True:
+    line = file_tic.readline()
+    if len(line) == 0: # Zero length indicates EOF
+      break
+    line = line.replace('\r', '')
+    line = line.replace('\n', '')
+    line = line.strip()
+    line_tic.append(line)
+  #for item in line_tic:
+  #  print item
+finally:
+  file_tic.close( )
+
+#read data
+try:
+  while True:
+    line = file_data.readline()
+    if len(line) == 0: # Zero length indicates EOF
+      break
+    line = line.replace('\r', '')
+    line = line.replace('\n', '')
+    line = line.strip()
+    line_data.append(line)
+
+  #for item in line_data:
+  #   print item
+  #tic_tmp = line_data[0].split('	')
+  #print tic_tmp[0]
+  #print tic_tmp[1]
+
+finally:
+  file_data.close()
+  file_tic.close()
+
+for tic_item in line_tic:
+  for data_item in line_data:
+    tic_tmp = []
+    tic_tmp = data_item.split('	')
+    if(tic_item == tic_tmp[0]):
+      print data_item
