e2e739111e3ee2c009d0ce5599df682340a13088
tsui <tsui.uestc@gmail.com>
2013-01-06 09:03:53 +0800

    add new task

--- crawl2/test.py
+++ crawl2/test.py
@@ -8,5 +8,11 @@ page_index = 1
 for page_index in range(1,4):
   re = 66 * page_index
   page_index_tmp = '%d' % re
+  page_index_tmm = '%d' % re*2
   print 'd' +  page_index_tmp
   page_index+=1
+  pa
+  
+
+
+
